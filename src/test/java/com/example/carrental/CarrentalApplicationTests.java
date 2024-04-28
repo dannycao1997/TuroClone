689e880f008a3556bea06bca880aca213de5e808
Danny Cao <dannycao1997@yahoo.com>
2024-04-28 17:21:51 -0400

    starting

---
+++ src/test/java/com/example/carrental/CarrentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

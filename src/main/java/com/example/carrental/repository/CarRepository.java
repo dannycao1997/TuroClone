dc7daa02035bef2d3b949db79f66252b72301315
Danny Cao <dannycao1997@yahoo.com>
2024-04-28 17:28:45 -0400

    created repository interfaces

---
+++ src/main/java/com/example/carrental/repository/CarRepository.java
@@ -0,0 +1,10 @@
+package com.example.carrental.repository;
+
+import com.example.carrental.entity.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long> {
+
+}
---
+++ src/main/java/com/example/carrental/repository/RentalRepository.java
@@ -0,0 +1,10 @@
+package com.example.carrental.repository;
+
+import com.example.carrental.entity.Rental;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RentalRepository extends JpaRepository<Rental, Long> {
+
+}
